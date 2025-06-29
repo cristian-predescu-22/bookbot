7f9d404a3710dd0e2cef5421a598791de6ad284e
Cristian Predescu <cristipredescu21@yahoo.com>
2025-06-29 11:24:44 +0300

    VsCode setup trying to check git commands and pushing

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path, encoding="utf-8") as f:
+        file_content = f.read()
+        return file_content
+
+
+def count(text):
+    pass
+
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+
+main()
