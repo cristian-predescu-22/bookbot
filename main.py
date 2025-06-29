879babe40d686a22be7f339305b71b5b2797faa5
Cristian Predescu <cristipredescu21@yahoo.com>
2025-06-29 18:17:21 +0300

    Finished the count for each word in a book

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-def get_book_text(file_path):
+def get_book_text(file_path: str) -> None:
+    total_words = {}
     with open(file_path, encoding="utf-8") as f:
         file_content = f.read()
-        return file_content
+        a = file_content.split()
+        for i in a:
+            if i not in total_words:
+                total_words[str(i)] = 1
+            else:
+                total_words[str(i)] += 1
+        sorted_total_words = dict(sorted(total_words.items(), key=lambda x: x[1]))
+        for i in sorted_total_words.keys():
+            print(f"{sorted_total_words[i]} -> {i}")
 
 
 def count(text):
